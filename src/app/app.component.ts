f375cbd38423b968106dced2f38f60f23e423786
ManuelaMejiaC <manuelamcastano@gmail.com>
2023-01-13 14:45:53 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild, OnInit} from '@angular/core';
+import {FormGroup, FormControl } from '@angular/forms';
+import { Chart } from 'chart.js/auto';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'plat_monitoreo_chec';
+export class AppComponent implements OnInit  {
+
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  channel = new FormGroup({
+    selectedChannel: new FormControl(),
+  });
+
+  responseData:any
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    //this.getData();
+  }
+
+  onDateChangeStart($event: any) {
+    this.range.get('start')?.setValue($event.value);
+    console.log(this.range.get('start'));
+  }
+
+  ondatechangeEnd($event: any) {
+    this.range.get('end')?.setValue($event.value);
+    console.log(this.range.get('end'));
+  }
+
+  @ViewChild('canvas')
+  canvas!: ElementRef;
+
+
+  aplicarFiltro(){
+    const url = 'http://localhost/server/monitoreo/getMonitoreoSomos.php';
+
+    const body = {
+      startDate: this.range.get('start')?.value,
+      endDate: this.range.get('end')?.value,
+      canalComunicacion: 'whatsapp'
+    }
+
+    this.http.post(url,body).subscribe((response:any) => {
+      this.responseData = response;
+
+      let labels: any[] = [];
+      let data: any[] = [];
+
+      this.responseData.respuesta1.forEach(function(item: any){
+        labels.push(item._id);
+        data.push(item.count);
+      });
+
+      const chartData = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Accesos al menú',
+            data: data,
+            backgroundColor: 'lightGreen',
+            borderColor: 'darkGreen',
+            borderWidth: 1
+          }
+        ]
+      };
+
+      const chart = new Chart(this.canvas.nativeElement, {
+        type: "bar",
+        data: chartData,
+        options: {}
+      });
+    })
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    FlexLayoutModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
